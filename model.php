b0f1c8a75334251e2d739d92561c88fee6fed7e2
rootDEV2990 <website.dev@icloud.com>
2021-07-25 17:34:20 -0500

    added sql connection to model

--- model.php
+++ model.php
@@ -1,3 +1,17 @@
 <?php
+    Class Model{
+        private $server = '127.0.0.1';//this is where you put the ip of your sql server
+        private $username = 'root';//this is your sql user
+        private $password = '';//this is for your login info
+        private $db = '';//this is for your db name
+        private $conn;
 
+        public function __construct(){
+            try {
+                $this->conn = new mysqli($this->server, $this->username. $this->password, $this->db);
+            } catch (Exception $e) {
+                echo "Connection error " . $e->getMessage();
+            }
+        }
+    }
 ?>
