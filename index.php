0a22175808d80e118a67de52214cd759d86fd59b
rootDEV2990 <website.dev@icloud.com>
2021-07-25 17:27:19 -0500

    first commit to feature branch added basic vue js files

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pvrBEATS - Chat</title>
 
-?>
+    <!-- Load required Bootstrap and BootstrapVue CSS -->
+    <link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap/dist/css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.css" />
+    <!--link rel="stylesheet" href="CSS/style.css"-->
+    <style>
+        [v-cloak] {
+            display: none;
+        }
+    </style>
+
+
+</head>
+<body>
+    <div class="container" id="app" v-cloak>
+        <div class="row">
+            <div class="col-md-12 mt-5">
+                <h1 class="text-center">PHP OOP VUEJS CRUD</h1>
+                <hr>
+            </div>
+        </div>
+    </div>
+
+    <!-- Load polyfills to support older browsers -->
+    <script src="//polyfill.io/v3/polyfill.min.js?features=es2015%2CIntersectionObserver" crossorigin="anonymous"></script>
+    <!-- Load Vue followed by BootstrapVue -->
+    <script src="//unpkg.com/vue@latest/dist/vue.min.js"></script>
+    <script src="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.js"></script>
+    <!-- Load the following for BootstrapVueIcons support -->
+    <script src="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue-icons.min.js"></script>
+    <!-- Axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!--script src="./JS/script.js"></script-->
+    <script>
+        var app = new Vue ({
+            el: '#app',
+            data: {
+                 
+            }
+        })
+    </script>
+</body>
+</html>
